eb93500b5c18a9761dbffcad29acfa84670a7dd7
xclcok <110390586+xclcok@users.noreply.github.com>
2023-04-06 15:13:44 +0000

    Add files via upload

---
+++ src/main/java/spring/ateliers/g2/atelier1/controllers/StudentController.java
@@ -0,0 +1,42 @@
+package com.dailycodework.sbqrcdoedemo.model.student;
+
+import com.dailycodework.sbqrcdoedemo.utils.QRCodeGenerator;
+
+import com.google.zxing.WriterException;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/students")
+public class StudentController {
+	@Autowired
+    private StudentService studentService;
+
+    @GetMapping
+    public ResponseEntity<List<Student>> getStudents() throws IOException, WriterException {
+        List<Student> students = studentService.getStudents();
+        if (students.size() != 0){
+            for (Student student : students){
+                QRCodeGenerator.generateQRCode(student);
+            }
+        }
+        return ResponseEntity.ok(studentService.getStudents());
+    }
+
+    @PostMapping
+    public Student addStudent(@RequestBody Student student){
+        return studentService.addStudent(student);
+    }
+
+    @GetMapping("/{id}")
+    public Student findById(@PathVariable("id") Long id){
+        return studentService.findById(id);
+    }
+}
